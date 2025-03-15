4f070a5bbed4b101a5c92fc7418bf974799e67db
Robin <robinghimire098@gmail.com>
2025-03-15 18:17:17 +0545

    Update and rename kathmanduheritage.html to kathmanduheritage.php

--- kathmanduheritage.php
+++ kathmanduheritage.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION["user_id"])) {
+    header("Location: index.html"); // Redirect to login if not logged in
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
